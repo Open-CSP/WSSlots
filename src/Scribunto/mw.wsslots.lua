07a1ca99072f34cef0b4d335bcef43f7664b1759
marijnvanwezel <marijnvanwezel@gmail.com>
2022-12-13 20:04:32 +0100

    Remove erroneous header that remained from the template

--- src/Scribunto/mw.wsslots.lua
+++ src/Scribunto/mw.wsslots.lua
@@ -1,12 +1,3 @@
---[[
-	Registers methods that can be accessed through the Scribunto extension
-
-	@since 0.1
-
-	@licence GNU GPL v2+
-	@author mwjames
-]]
-
 -- Variable instantiation
 local slots = {}
 local php
@@ -17,13 +8,14 @@ function slots.setupInterface()
     php = mw_interface
     mw_interface = nil
 
-    -- Register library within the "mw.smw" namespace
+    -- Register library within the "mw.slots" namespace
     mw = mw or {}
     mw.slots = slots
 
     package.loaded['mw.slots'] = slots
 end
 
+-- slotContent
 function slots.slotContent( slotName, pageName )
     if not type( slotName ) == 'string' or not type( pageName ) == 'string' or not type( pageName ) == 'nil' then
         error( 'Invalid parameter type supplied to mw.slots.slotContent()' )
