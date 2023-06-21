7548334c58d772e55dacaff6572e9cea7e457255
Wout Gevaert <wout@wikibase.nl>
2023-06-21 17:07:19 +0200

    Do not remove subobject keys etc from semanticdata
    This makes stuff go wrong

--- src/WSSlotsHooks.php
+++ src/WSSlotsHooks.php
@@ -12,6 +12,7 @@ use MediaWiki\MediaWikiServices;
 use MediaWiki\ResourceLoader\Hook\ResourceLoaderGetConfigVarsHook;
 use MWException;
 use RequestContext;
+use SMW\DIContainer;
 use SMW\ParserData;
 use SMW\SemanticData;
 use SMW\Store;
@@ -184,8 +185,13 @@ class WSSlotsHooks implements
 
 			// Remove any pre-defined properties that exist in both the main semantic data as well as the slot semantic
 			// data from the main semantic data to prevent them from merging
+			// Except for DIContainers, because these _should_ be merged; the subsemanticdata is saved anyway, so deleting their
+			// relation to the semantic data is a bad idea.
 			foreach ( $slotSemanticData->getProperties() as $property ) {
-				if ( !$property->isUserDefined() ) {
+				if (
+					!( $property instanceof DIContainer )
+					&& !$property->isUserDefined()
+				) {
 					$semanticData->removeProperty( $property );
 				}
 			}
